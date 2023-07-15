de341f9481be0cc1d4a58cf04de5d3bc59b2d4b0
Selim Altayev <selim.altayev@gmail.com>
2023-07-15 15:05:42 +0200

    made faq section layout

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,10 +1,13 @@
 import AboutSection from "../components/AboutSection"
 import SecvicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
+
 function AboutUs() {
     return (
         <div className="AboutUs">
             <AboutSection />
             <SecvicesSection />
+            <FaqSection />
         </div>
     )
 }
