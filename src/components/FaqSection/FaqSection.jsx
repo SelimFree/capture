90b9383f01c51ad12dc9575f6e21471a73445914
Selim Altayev <selim.altayev@gmail.com>
2023-07-20 14:56:11 +0200

    added faq toggle animation

--- src/components/FaqSection/FaqSection.jsx
+++ src/components/FaqSection/FaqSection.jsx
@@ -1,4 +1,8 @@
-import "./FaqSection.scss"
+import Toggle from "../Toggle/Toggle";
+import "./FaqSection.scss";
+
+//Animation
+import { LayoutGroup } from "framer-motion";
 
 function FaqSection() {
     return (
@@ -6,50 +10,53 @@ function FaqSection() {
             <h2>
                 Any question? <span>FAQ</span>
             </h2>
-            <div className="question">
-                <h4>How do I start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Esse nostrum earum voluptatum ullam ex nisi!
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Esse nostrum earum voluptatum ullam ex nisi!
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different payment methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Esse nostrum earum voluptatum ullam ex nisi!
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Esse nostrum earum voluptatum ullam ex nisi!
-                    </p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+            <LayoutGroup>
+                <Toggle title="How do I start?">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Esse nostrum earum voluptatum ullam ex nisi!
+                        </p>
+                    </div>
+                </Toggle>
+            </LayoutGroup>
+
+            <LayoutGroup>
+                <Toggle title="What products do you offer">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Esse nostrum earum voluptatum ullam ex nisi!
+                        </p>
+                    </div>
+                </Toggle>
+            </LayoutGroup>
+
+            <LayoutGroup>
+                <Toggle title="Daily Schedule">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Esse nostrum earum voluptatum ullam ex nisi!
+                        </p>
+                    </div>
+                </Toggle>
+            </LayoutGroup>
+
+            <LayoutGroup>
+                <Toggle title="Different payment methods">
+                    <div className="answer">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            Esse nostrum earum voluptatum ullam ex nisi!
+                        </p>
+                    </div>
+                </Toggle>
+            </LayoutGroup>
         </div>
     );
 }
