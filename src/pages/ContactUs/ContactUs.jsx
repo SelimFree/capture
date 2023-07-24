f7387b354d9eaf960ac4c59de97826d31b2d6244
Selim Altayev <selim.altayev@gmail.com>
2023-07-24 14:21:12 +0200

    added contact us layout and animation

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,7 +1,37 @@
+import "./ContactUs.scss"
 
+//Animation
+import { motion } from "framer-motion"
+import { titleAnimation } from "../../animation"
 function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div className="ContactUs" style={{background: "#fff"}}>
+      <div className="title">
+        <div className="hide">
+          <motion.h2 variants={titleAnimation}>
+            Get in touch.
+          </motion.h2>
+        </div>
+      </div>
+        <div className="hide">
+          <motion.div className="social" variants={titleAnimation}>
+            <div className="circle"></div>
+            <h2>Send us messaage</h2>
+          </motion.div>
+        </div>
+        <div className="hide">
+          <motion.div className="social" variants={titleAnimation}>
+            <div className="circle"></div>
+            <h2>Follow us on Twitter</h2>
+          </motion.div>
+        </div>
+        <div className="hide">
+          <motion.div className="social" variants={titleAnimation}>
+            <div className="circle"></div>
+            <h2>Follow us on Instagramm</h2>
+          </motion.div>
+        </div>
+    </div>
   )
 }
 
