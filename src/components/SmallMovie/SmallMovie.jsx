4eba051152aa2fa9e43bf22a13518105aec83ca7
Selim Altayev <selim.altayev@gmail.com>
2023-07-24 13:49:43 +0200

    added small movies scroll animation

--- src/components/SmallMovie/SmallMovie.jsx
+++ src/components/SmallMovie/SmallMovie.jsx
@@ -4,10 +4,12 @@ import { Link } from "react-router-dom";
 //Animation
 import { motion } from "framer-motion";
 import { fade, photoAnimation, lineAnimation } from "../../animation"
+import {useScroll} from "../../hooks/useScroll"
 
 function SmallMovie({movie}) {
+    const [element, controls] = useScroll()
     return (
-        <div className="SmallMovie">
+        <motion.div className="SmallMovie" ref={element} variants={fade} animate={controls} initial="hide">
             <motion.h2 variants={fade}>{movie.title}</motion.h2>
             <motion.div variants={lineAnimation} className="line"></motion.div>
             <Link to={movie.url}>
@@ -15,7 +17,7 @@ function SmallMovie({movie}) {
                     <motion.img variants={photoAnimation} src={movie.mainImg} />
                 </div>
             </Link>
-        </div>
+        </motion.div>
     );
 }
 
