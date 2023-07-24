7928bcc9d7e10bfcae8ef906866e4eb31874d0bf
Selim Altayev <selim.altayev@gmail.com>
2023-07-24 15:42:55 +0200

    navbar line animation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
 import "./Navbar.scss";
 //Router Link
-import { Link } from "react-router-dom";
-
+import { Link, useLocation } from "react-router-dom";
+//Animation
+import { motion } from 'framer-motion'
 function Navbar() {
+    const {pathname} = useLocation();
+    console.log(pathname)
     return (
         <nav className="Navbar">
             <Link to={"/"}>
@@ -16,16 +19,19 @@ function Navbar() {
                 <Link to={"/"}> 
                     <li>
                         <p>About Us</p>
+                        <motion.div className="line" transition={{duration: 0.75}} initial={{width: "0%"}} animate={{width: pathname === "/" ? "50%" : "0%"}}></motion.div>
                     </li>
                 </Link>
                 <Link to={"/work"}> 
                     <li>
                         <p>Our Work</p>
+                        <motion.div className="line" transition={{duration: 0.75}} initial={{width: "0%"}} animate={{width: pathname === "/work" ? "50%" : "0%"}}></motion.div>
                     </li>
                 </Link>
                 <Link to={"/contact"}> 
                     <li>
                         <p>Contact Us</p>
+                        <motion.div className="line" transition={{duration: 0.75}} initial={{width: "0%"}} animate={{width: pathname === "/contact" ? "50%" : "0%"}}></motion.div>
                     </li>
                 </Link>
             </ul>
